be440e8fe72988a2b2caa319bccd1d889b2810c8
Subbulakshmi <subbulakshmi.arumugam20@gmail.com>
2023-12-29 12:20:16 -0500

    added image commit

---
+++ app/src/main/java/com/example/onelinerapp/Images.java
@@ -0,0 +1,18 @@
+package com.example.onelinerapp;
+
+
+
+class Result{
+    Urls urls;
+}
+class Urls{
+    public String getRegular() {
+        return regular;
+    }
+
+    public void setRegular(String regular) {
+        this.regular = regular;
+    }
+
+    String regular;
+}
